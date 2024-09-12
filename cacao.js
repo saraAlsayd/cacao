30386c5494c076dced7474991ede919ed8202342
saraAlsayd <roraya2442@gmail.com>
2024-09-12 15:36:39 +0200

    up2

---
+++ cacao.js
@@ -0,0 +1,10 @@
+let mainI = document.querySelector('.mainI');
+let container = document.querySelector('.container');
+
+function choco (x){
+    mainI.src = x;
+}
+
+function color (color){
+    container.style.background = color;
+}
